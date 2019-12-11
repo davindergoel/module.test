3e9072b97c890e0388c841d3c4cb30a30f9cc9cc
Davinder Goel <davinder.goel@debutinfotech.com>
2019-12-11 11:02:13 +0530

    Test

--- app/src/main/java/com/demo/modulegradle/MainActivity.kt
+++ app/src/main/java/com/demo/modulegradle/MainActivity.kt
@@ -10,8 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
         Log.e("Result", TestManager.getInstance(this)?.subtract(112, 20).toString() + "::")
     }
 }
